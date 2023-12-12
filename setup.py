83da3d49fce42bfea91814305ccf210e30d2bce0
hmedal <hugh.medal@gmail.com>
2023-12-12 09:56:16 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
       description='Utilities for research in the Medal Group',
       author='The Medal Group',
       author_email='hmedal@utk.edu',
-      url='https://github.com/medalgrouputk/util/',      
+      url='https://github.com/medalgrouputk/medalgrouputils/',      
       install_requires=[ 
             "IPython",
             "matplotlib",
